9dde7baf675bf2b0a50b1599ce655fe7f0c11d2e
TruongAn202 <nguyenvantruongan7745@gmail.com>
2024-07-21 15:19:02 +0700

    8. Setup camera

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-
+import React, { useRef, useEffect } from 'react';
 import './App.css';
-import {Howl, Howler} from 'howler';
-import soundURL from './assets/Giong-nu-noi-xin-chao-www_tiengdong_com.mp3'
+import { Howl, Howler } from 'howler';
+import soundURL from './assets/Giong-nu-noi-xin-chao-www_tiengdong_com.mp3';
 
 // var sound = new Howl({
 //   src: [soundURL]
@@ -10,9 +10,53 @@ import soundURL from './assets/Giong-nu-noi-xin-chao-www_tiengdong_com.mp3'
 //sound.play();
 
 function App() {
+
+  const videoRef = useRef(null);
+
+  const init = async () => {
+    console.log('init...');
+    await setupCamera();
+  }
+
+  const setupCamera = () => {
+    return new Promise((resolve, reject) => {
+      const getUserMedia = (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia;
+
+      // if (getUserMedia) {
+      //   getUserMedia.call(navigator.mediaDevices || navigator, { video: true }) // quyen truy cap
+      //     .then(stream => {
+      //       if (videoRef.current) { 
+      //         videoRef.current.srcObject = stream;
+      //         videoRef.current.addEventListener('loadeddate',resolve)
+      //       }
+      //       resolve();
+      //     })
+      //     .catch(error => reject(error));
+      // } else {
+      //   reject(new Error("getUserMedia is not supported in this browser"));
+      // }
+    });
+  }
+
+  useEffect(() => {
+    init();
+
+    // Cleanup
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        const tracks = videoRef.current.srcObject.getTracks();
+        tracks.forEach(track => track.stop());
+      }
+    }
+  }, []);
+
   return (
     <div className="main">
       <video
+        ref={videoRef}
         className='video'
         autoPlay
       />
